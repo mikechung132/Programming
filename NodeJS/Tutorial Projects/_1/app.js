b16e4ba6d133e0f3ef1a33e7a9b67d162f6f6355
mikechung132 <44271908+mikechung132@users.noreply.github.com>
2019-04-10 12:30:04 -0400

    Update Folder

--- NodeJS/Tutorial Projects/_1/app.js
+++
@@ -1,7 +0,0 @@
-// Tutorial 1 - Module Exports
-
-const tutorial = require('./tutorial');
-console.log(tutorial.sum(1,3));
-console.log(tutorial.PI);
-console.log(new tutorial.SomeMathObject());
-//console.log();
--- NodeJS/Tutorial Projects/_5/stream.js
+++
@@ -1,8 +0,0 @@
-// Tutorial 5 - Why you should use Streams
-
-const fs = require('fs');
-const readStream = fs.createReadStream('./largeFile.txt', 'utf8');
-
-readStream.on('data', (chunk) => {
-      console.log(chunk);
-});
